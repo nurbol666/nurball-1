35c9ee9f45cfea7ed336e860a6b249092e3785d8
nurbol666 <47814405+nurbol666@users.noreply.github.com>
2024-02-06 00:58:18 +0600

    Add files via upload
    First upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const qs = (selector) => document.querySelector(selector);
+const question = qs(".question");
+const gif = qs(".gif");
+const [yesBtn, noBtn] = [".yes-btn", ".no-btn"].map(qs);
+
+const handleYesClick = () => {
+  question.innerHTML = "Yeahhhhhhhhhhh! I love you so much!I had no doubt!!";
+  gif.src = "https://media.tenor.com/P-VyTymrVJgAAAAi/bubu-dudu.gif";
+
+  // Remove the 'mouseover' event listener from noBtn
+  noBtn.removeEventListener("mouseover", handleNoMouseOver);
+
+  // Remove the noBtn from the DOM
+  noBtn.remove();
+
+  // Define predefined romantic date ideas
+  const dateIdeas = [
+    "I love you my BUBU <3",
+
+    // Add more date ideas as needed
+  ];
+
+  // Create and style a new button for Let's Go!
+  const letsGoBtn = document.createElement("button");
+  letsGoBtn.textContent = "Let's Go!";
+  letsGoBtn.classList.add("letsgo-btn"); // You can add a class for styling if needed
+  letsGoBtn.style.position = "absolute";
+
+  // Adjust the left position based on screen width
+  if (window.innerWidth <= 800) {
+    letsGoBtn.style.left = "95%";
+  } else {
+    letsGoBtn.style.left = "63%";
+  }
+
+  letsGoBtn.style.transform = "translate(-50%, -50%)";
+  letsGoBtn.style.width = "200px"; // Adjust the width as needed
+
+  // Add a click event listener to prompt the user with random romantic date ideas
+  letsGoBtn.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * dateIdeas.length);
+    const selectedDateIdea = dateIdeas[randomIndex];
+
+    alert(`Message from you DUDU : ${selectedDateIdea}`);
+  });
+
+  // Replace yesBtn with the new letsGoBtn
+  yesBtn.replaceWith(letsGoBtn);
+};
+
+const handleNoMouseOver = () => {
+  const { width, height } = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - width;
+  const maxY = window.innerHeight - height;
+
+  noBtn.style.left = `${Math.floor(Math.random() * maxX)}px`;
+  noBtn.style.top = `${Math.floor(Math.random() * maxY)}px`;
+};
+
+yesBtn.addEventListener("click", handleYesClick);
+noBtn.addEventListener("mouseover", handleNoMouseOver);
